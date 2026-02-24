3dc6fed1253cc206e1dd675e1d0766ee878cd405
drusin <dawidrusin90@gmail.com>
2026-02-24 12:34:17 +0100

    added persistence to words store

--- src/stores/words.ts
+++ src/stores/words.ts
@@ -1,31 +1,34 @@
 import { defineStore } from "pinia";
 import allWords from "@/words/allWords.ts";
+import { computed, ref, type Ref, watch } from "vue";
 
-export const useWordsStore = defineStore('words', {
-  state: () => {
-    return {
-      used: [] as Array<string>,
-    };
-  },
-  actions: {
-    reset() {
-      this.used.length = 0;
-    },
-    nextWord() {
-      const all = allWords();
-      if (this.used.length === all.length) {
-        return '';
-      }
-      let word = '';
-      do {
-        const i = Math.floor(Math.random() * all.length);
-        word = all[i] || '';
-      } while (!word && !this.used.find((w) => w === word));
-      this.used.push(word);
-      return word;
+const USED_KEY = 'words.used';
+
+export const useWordsStore = defineStore('words', () => {
+  const used: Ref<Array<string>> = ref(JSON.parse(localStorage.getItem(USED_KEY)!) || []);
+  watch(used.value, (val) => localStorage.setItem(USED_KEY, JSON.stringify(val)));
+
+  const hasNext = computed(() => used.value.length < allWords().length);
+
+  function reset() {
+    used.value.length = 0;
+  }
+
+  function nextWord() {
+    const all = allWords();
+    if (used.value.length === all.length) {
+      return '';
     }
-  },
-  getters: {
-    hasNext: (state) => state.used.length < allWords().length,
+    let word = '';
+    do {
+      const i = Math.floor(Math.random() * all.length);
+      word = all[i] || '';
+    } while (!word && !used.value.find((w) => w === word));
+    used.value.push(word);
+    return word;
+  }
+
+  return {
+    used, hasNext, reset, nextWord
   }
 });
